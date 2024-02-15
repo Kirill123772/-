48971d40f175e9584cb0688ed3ab07db5aeb6254
student <kiriltoster45@gmail.com>
2024-02-15 18:35:54 +0200

    Init

---
+++ main.py
@@ -0,0 +1,40 @@
+while True:
+    a1 = input('Імя:')
+    if a1.isalpha():
+        break
+    else:
+        print('Помилка')
+while True:
+    a2 = input('Прізвище:')
+    if a2.isalpha():
+        break
+    else:
+        print('Помилка')
+while True:
+    a3 = input('По батькові:')
+    if a3.isalpha():
+        break
+else:
+    print('Помилка')
+print(f"{a2.capitalize()} {a1[0].capitalize()}.{a3[0].title()}.")
+
+
+
+a = input('Введіть строку:')
+b = a [  : : -1]
+print(b)
+
+
+
+a = input('Введіть рядок:')
+letter = 0
+chisla = 0
+for i in a:
+    if a == letter:
+     letter += 1
+
+
+    print(b)
+
+
+
