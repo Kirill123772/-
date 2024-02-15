01714e1a731933aef6e38275d33e88672227fca3
student <Kiriltoster45@gmail.com>
2024-02-15 19:06:28 +0200

    Removed variable"a"

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ while True:
         print('Помилка')
 while True:
     a2 = input('Прізвище:')
-    if a2.i
+    if a2.isalpha():
+        break
+    else:
+        print('Помилка')
+while True:
+    a3 = input('По батькові:')
+    if a3.isalpha():
+        break
+else:
+    print('Помилка')
+C:\Users\student\PycharmProjects\pythonProject\.idea\workspace.xml
 
 
 
